25e3e6b0ba1fe3424d0f7ec436e85dbcd3682a44
Jordi Prats <jprats@systemadmin.es>
2017-10-23 17:16:38 +0200

    refresh doc Mon Oct 23 17:16:38 CEST 2017

--- update-docs.sh
+++ update-docs.sh
@@ -17,10 +17,12 @@ do
 	fi
 done
 
+echo "<html><head></head>" > $2/index.html
 for i in $(find $2 -maxdepth 1 -type d | grep -v "^.git$");
 do
   echo "<a href=\"$i\">$i</a>" >> $2/index.html
 done
+echo "</html>" > $2/index.html
 
 cd $2
 git add --all
